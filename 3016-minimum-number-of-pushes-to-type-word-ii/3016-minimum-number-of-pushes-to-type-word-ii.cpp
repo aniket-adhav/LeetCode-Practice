4f60e55e20571bebfa7f20768f58018d0b5b6f61
Aniket Adhav <aniketadhav2006@gmail.com>
2026-07-31 14:34:15 +0530

    Time: 4 ms (86.59%), Space: 25.5 MB (61.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,6 +4,7 @@ public:
         int res = 0, freq[26] = {};
         for (char c : word)
             freq[c-'a']++;
+            
         sort(begin(freq), end(freq), greater<int>());
         for (int i = 0; i < 26 && freq[i]; i++)
             res += freq[i] * (i / 8 + 1);
